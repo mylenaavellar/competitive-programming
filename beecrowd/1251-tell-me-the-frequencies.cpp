5a03297416f23df5ab4c7de5e915b279fa86d6ab
Mylena Avellar <mylenatavellar@gmail.com>
2026-03-25 22:40:21 -0300

    fix: corrige questao 1251 beecrowd

--- beecrowd/1251-tell-me-the-frequencies.cpp
+++ beecrowd/1251-tell-me-the-frequencies.cpp
@@ -4,14 +4,21 @@
 #include <algorithm>
 
 int main() {
-    char texto[1000];
+    char texto[1001];
     std::vector<std::pair<int,int>> pares;
+    int caso = 0;
     
     while (std::cin.getline(texto, sizeof(texto))) {
+        if (caso > 0) {
+            std::cout << "\n";
+        }
+        ++caso;
+
+        int len = strlen(texto);
         int ascii[128] = {0};
         pares.clear();
 
-        for (int i = 0; i < strlen(texto); ++i) {
+        for (int i = 0; i < len; ++i) {
             ascii[int(texto[i])]++;
         }
 
@@ -23,14 +30,16 @@ int main() {
             pares.push_back({ascii[i], i});
         }
 
-        std::sort(pares.begin(), pares.end());
-
+        // Frequência ascendente. Em caso de empate: maior ASCII primeiro
+        std::sort(pares.begin(), pares.end(), [](const auto& a, const auto& b) {
+            if (a.first != b.first) return a.first < b.first;
+            return a.second > b.second;
+        });
+        
         for (const auto& p : pares) {
             std::cout << p.second << " " << p.first << "\n";
         }
-
-        std::cout << "\n";
     }
-    
+
     return 0;
 }
